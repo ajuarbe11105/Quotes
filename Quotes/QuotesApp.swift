43e12bb12da79e106c9790b0add315aefee950aa
ajuarbe11105 <63558235+ajuarbe11105@users.noreply.github.com>
2022-03-17 22:14:28 -0400

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Alberto Juarbe on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
