89aefca0d700b792cd9085e15786a469de53e063
ajuarbe11105 <63558235+ajuarbe11105@users.noreply.github.com>
2022-03-20 17:41:28 -0400

    -add View

--- Quotes/Views/GandhiDetail.swift
+++ Quotes/Views/GandhiDetail.swift
@@ -9,39 +9,28 @@ import SwiftUI
 
 struct GandhiDetail: View {
     
-    //var gandhi: [Gandhi] = Quotes.gandhiQotes
-    //var gandhi: Gandhi
+    
     var gandhi: [Gandhi] = Quotes.gandhiQotes
     
-   
+    
     
     var body: some View {
         ScrollView(showsIndicators: false) {
-        VStack {
-        ForEach(gandhi, id: \.id) { item in
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.gray)
-                .shadow(color: .black, radius: 10, x: 10, y: 10)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                .overlay(
-                    Text(item.quote)
-
-                )
-//            RoundedRectangle(cornerRadius: 20)
-//                .fill(.white)
-//                .shadow(color: .purple, radius: 10)
-//
-//                .padding()
-//                .overlay(
-//                    Text(item.quote)
-//                .font(.caption)
-//                .foregroundColor(.black)
-//                .padding()
-//                .padding()
-//            )
-//            Spacer()
-        }
-        }
+            VStack {
+                ForEach(gandhi, id: \.id) { item in
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.gray)
+                        .frame(width: 400, height: 150)
+                        .shadow(color: .black, radius: 2, x: 2, y: 2)
+                        .overlay(
+                            Text(item.quote)
+                            
+                        )
+                        .padding(10)
+                    
+                    
+                }
+            }
         }
     }
 }
